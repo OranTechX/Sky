d4b02bde01ce8e2ca43f132bb3f3f4d8468281b0
RitaHuang9 <rr86916@gmail.com>
2026-03-28 16:11:46 +0800

    feat: 完成多國語系翻譯、修正 UI 樣式及 i18n 電子郵件格式報錯

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,15 +1,20 @@
 <template>
   <h1 class="text-3xl font-bold mt-10 text-center">{{ $t("nav.about") }}</h1>
-  <div class="bg-white w-[80%] mt-10 p-6 rounded-lg text-center m-auto leading-8 text-gray-500">
+  <div
+    class="bg-white w-[80%] mt-10 p-6 rounded-lg text-center m-auto leading-8 text-gray-500"
+  >
     <p>
-      {{ $t('aboutPage.description_part1') }}
+      {{ $t("aboutPage.description_part1") }}
     </p>
 
     <h6 class="text-xl font-bold italic text-secondary-500 my-4">
-      {{ $t('aboutPage.mission') }}
+      {{ $t("aboutPage.mission") }}
     </h6>
     <p v-if="$te('aboutPage.description_part2')">
-  {{ $t('aboutPage.description_part2') }}
-</p>
+      {{ $t("aboutPage.description_part2") }}
+    </p>
+    <h6 class="mt-6 pt-6 border-t border-gray-300">
+    {{ $t('aboutPage.companyInfo') }}
+  </h6>
   </div>
 </template>
--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,25 +1,34 @@
-
 <template>
   <h1 class="text-3xl font-bold mt-10 text-center">{{ $t("nav.contact") }}</h1>
-  <div class="bg-white w-[80%] mt-10 p-6 rounded-lg text-center m-auto leading-8 text-gray-500">
-    <p class="text-gray-600 leading-relaxed text-left">無論您是有具體的專案需求，還是想了解更多技術細節，我們都隨時準備好為您提供支援。<br/>請填寫下方的表單，我們的技術顧問將在 24 小時內與您聯繫。</p>
+  <div
+    class="bg-white w-[80%] mt-10 p-6 rounded-lg text-center m-auto leading-8 text-gray-500"
+  >
+    <p class="text-gray-600 leading-relaxed text-left">
+      {{ $t("contactPage.description1") }}<br />{{ $t("contactPage.description2") }}
+    </p>
     <form @submit.prevent="handleSubmit" class="space-y-6 mt-5">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div>
-          <label class="block text-sm font-semibold text-gray-700 mb-2 text-left">姓名</label>
+          <label
+            class="block text-sm font-semibold text-gray-700 mb-2 text-left"
+            >{{ $t("contactPage.form.name") }}</label
+          >
           <input
             type="text"
-            placeholder="您的稱呼"
+            :placeholder="$t('contactPage.form.namePlaceholder')"
             class="w-full px-4 py-3 rounded-lg border border-gray-200 focus:ring-2 focus:ring-secondary-500 focus:border-transparent outline-none transition-all"
             required
           />
         </div>
 
         <div>
-          <label class="block text-sm font-semibold text-gray-700 mb-2 text-left">電子郵件</label>
+          <label
+            class="block text-sm font-semibold text-gray-700 mb-2 text-left"
+            >{{ $t("contactPage.form.email") }}</label
+          >
           <input
             type="email"
-            placeholder="example@mail.com"
+            :placeholder="$t('contactPage.form.emailPlaceholder')"
             class="w-full px-4 py-3 rounded-lg border border-gray-200 focus:ring-2 focus:ring-secondary-500 focus:border-transparent outline-none transition-all"
             required
           />
@@ -27,10 +36,12 @@
       </div>
 
       <div>
-        <label class="block text-sm font-semibold text-gray-700 mb-2 text-left">說明</label>
+        <label class="block text-sm font-semibold text-gray-700 mb-2 text-left"
+          >{{ $t("contactPage.form.message") }}</label
+        >
         <textarea
           rows="4"
-          placeholder="請簡述您的問題或需求..."
+          :placeholder="$t('contactPage.form.messagePlaceholder')"
           class="w-full px-4 py-3 rounded-lg border border-gray-200 focus:ring-2 focus:ring-secondary-500 focus:border-transparent outline-none transition-all resize-none"
           required
         ></textarea>
@@ -38,18 +49,17 @@
 
       <button
         type="submit"
-        class="w-45 py-2 bg-secondary-500 hover:bg-secondary-700 text-white rounded-lg "
+        class="w-45 py-2 bg-secondary-500 hover:bg-secondary-700 text-white rounded-lg"
       >
-        確認送出
+      {{ $t("contactPage.form.submit") }}
       </button>
     </form>
-
   </div>
 </template>
 
 <script setup>
 const handleSubmit = () => {
   // 這裡之後可以寫發信邏輯
-  alert('訊息已收到，我們會盡快與您聯繫！');
+  alert("訊息已收到，我們會盡快與您聯繫！");
 };
 </script>
